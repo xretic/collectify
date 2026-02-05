9c7313eaf9f98b312da1b2415701743c7de89394
xretic <flourchik@gmail.com>
2026-02-05 17:21:29 +0100

    chores

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,17 @@
-import 'dotenv/config';
 import { PrismaPostgresAdapter } from '@prisma/adapter-ppg';
 import { PrismaClient } from '@/generated/prisma/client';
 
 const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-    throw new Error('DATABASE_URL not set in .env');
-}
+if (!connectionString) throw new Error('DATABASE_URL is not set');
 
 const adapter = new PrismaPostgresAdapter({ connectionString });
-export const prisma = new PrismaClient({ adapter });
+
+const globalForPrisma = globalThis as unknown as {
+    prisma?: PrismaClient;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
