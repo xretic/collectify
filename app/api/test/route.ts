92a7cfce07f8ce785c07f6f9add6ee79ccd9b67b
xretic <flourchik@gmail.com>
2026-02-06 23:06:16 +0100

    fix: delete test folder

--- app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { redis } from '@/lib/redis';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-    await redis.set('hello', 'world', { ex: 60 });
-    const value = await redis.get('hello');
-
-    return NextResponse.json({ value });
-}
