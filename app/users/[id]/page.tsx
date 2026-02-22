e6a2f4f3f420420209f4d19ceb4b1b96ba1bd320
xretic <flourchik@gmail.com>
2026-02-22 21:12:34 +0100

    feat: change follow button color to inherit

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -193,8 +193,7 @@ export default function ProfilePage() {
                         <Tooltip title={followed ? 'Unfollow' : 'Follow'}>
                             <IconButton
                                 onClick={toggleFollow}
-                                size="small"
-                                color="error"
+                                color="inherit"
                                 disabled={!user || debouncedFollowed !== followed}
                             >
                                 {followed ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
