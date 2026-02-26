edde606ed80932b694513cb5d0c3711db1aabe67
xretic <flourchik@gmail.com>
2026-02-26 21:10:23 +0100

    chores: delete old socket types

--- types/next-socketio.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Server as HttpServer } from 'http';
-import type { Server as IOServer } from 'socket.io';
-
-declare module 'http' {
-    interface Server {
-        io?: IOServer;
-    }
-}
